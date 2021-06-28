5a38c4a0d4ba1e263db737abf9f6f0b84679ca22
carlosamr2 <carlosamr2@hotmail.com>
2021-06-27 22:22:13 -0500

    nombres

--- sintactical.py
+++ sintactical.py
@@ -5,7 +5,7 @@ import ply.yacc as yacc
 # Get the token map from the lexer.  This is required.
 from main import tokens
 
-def p_expression_expr(p):
+def p_expression_expr(p):  #Carlos Moncayo y Maria Rivera
     '''expression : variable
     | variable expression
     | dataStruct
@@ -13,67 +13,59 @@ def p_expression_expr(p):
     | controlStruct
     | controlStruct expression'''
 
-def p_variable_expr(p):
+def p_variable_expr(p): #Carlos Moncayo y Maria Rivera
     'variable : type NAME IGUAL datatype SEMICOLON'
 
-def p_controlStruct_expr(p):
+def p_controlStruct_expr(p): #Carlos Moncayo y Maria Rivera
     'controlStruct : while'
 
-def p_dataStruct_expr(p):
+def p_dataStruct_expr(p): #Carlos Moncayo y Maria Rivera
     'dataStruct : array'
 
-def p_while_expr(p):
+def p_while_expr(p):   #Carlos Moncayo
     '''while : WHILE OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACE expression CLOSE_BRACE
     | WHILE OPEN_PARENTHESIS bool CLOSE_PARENTHESIS OPEN_BRACE expression CLOSE_BRACE'''
 
-def p_operations(p):
+def p_operations(p): #Carlos Moncayo y Maria Rivera
     '''operations : NUMBER operand NUMBER'''
 
-def p_datatype_expr(p):
+def p_datatype_expr(p):  #Carlos Moncayo y Maria Rivera
     '''datatype : NUMBER
     | STRING
     | operations
     | CHAR'''
 
-def p_operand_expr(p):
+def p_operand_expr(p):    #Carlos Moncayo
     '''operand : SUMA 
     | RESTA 
     | MULTIPLICACION 
     | DIVISION'''
 
-def p_bool_expr(p):
+def p_bool_expr(p):   #Carlos Moncayo
     '''bool : TRUE
     | FALSE'''
 
-def p_type_expr(p):
+def p_type_expr(p):   #Carlos Moncayo
     '''type : CONST
     | LET
     | VAR'''
 
-""" def p_object_expr(p):
-    '''object : char
-    | array
-    | map
-    | set
-    | NUMBER
-    | STRING
-    | NULL''' """
 
-def p_array_expr(p):
+def p_array_expr(p):    #Carlos Moncayo
     '''array : type NAME IGUAL OPEN_BRACKET items CLOSE_BRACKET SEMICOLON
     | type NAME IGUAL NEW ARRAY OPEN_PARENTHESIS items CLOSE_PARENTHESIS SEMICOLON'''
 
 
-def p_items_expr(p):
+def p_items_expr(p):  #Carlos Moncayo y Maria Rivera
     '''items : numeros
     | cadena'''
 
-def p_numeros_expr(p):
+def p_numeros_expr(p):   #Carlos Moncayo y Maria Rivera
     '''numeros : NUMBER
     | NUMBER COMMA numeros'''
 
-def p_cadena_expr(p):
-    '''cadena : STRING
+def p_cadena_expr(p):   #Carlos Moncayo y Maria Rivera
+    '''cadena : STRING 
     | STRING COMMA cadena'''
 
 # Error rule for syntax errors
@@ -92,16 +84,3 @@ while True:
         continue
     result = parser.parse(s)
     print(result)
-
-""" "
-expression: expression + term
-| expression - term
-| term
-
-term: term * factor
-| term / factor
-| factor
-
-factor: NUMBER
-| (expression)
- """
